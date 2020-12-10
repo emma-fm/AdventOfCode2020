cfe8d08ce76001443c23bde3055bb225cdd9e0d8
Emma Forner <emma-fm@pop-os.localdomain>
2020-12-10 22:44:49 +0100

    day 10 part

--- README.md
+++ README.md
@@ -4,7 +4,3 @@ Rust solutions for the Advent of Code challenge of the year 2020
 
 ## Input
 To solve a problem you need that day's input, located as /inputs/dayXX at the project's root folder.
-
-## Days missing
-List of problems currently missing:
-* Day 10 part 2
--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -23,4 +23,54 @@ fn main() {
     }
 
     println!("Differences multiplied: {}", one * three);
+
+    let mut cache : Vec<usize> = vec!(0; 94);
+
+    let res = calc_from(&values, 0, &mut cache);
+
+    println!("{}", res);
 }
+
+fn calc_from(values: &Vec<usize>, i: usize, mut cache: &mut Vec<usize>) -> usize {
+    if i == values.len() - 1 {
+        return 1;
+    }
+    else {
+        let mut size = 0;
+        let v = values[i];
+        if i + 1 < values.len() && (values[i+1] == v + 1 || values[i+1] == v + 2 || values[i+1] == v + 3) {
+            if cache[i+1] != 0 {
+                size += cache[i+1];
+            }
+            else {
+                let val = calc_from(&values, i+1, cache);
+                cache[i+1] = val;
+                size += val;
+            }
+            
+        }
+        if i + 2 < values.len() && (values[i+2] == v + 1 || values[i+2] == v + 2 || values[i+2] == v + 3) {
+            if cache[i+2] != 0 {
+                size += cache[i+2];
+            }
+            else {
+                let val = calc_from(&values, i+2, cache);
+                cache[i+2] = val;
+                size += val;
+
+            }
+        }
+        if i + 3 < values.len() && (values[i+3] == v + 1 || values[i+3] == v + 2 || values[i+3] == v + 3) {
+            if cache[i+3] != 0 {
+                size += cache[i+3];
+            }
+            else {
+                let val = calc_from(&values, i+3, cache);
+                cache[i+3] = val;
+                size += val;
+            }
+        }
+
+        size
+    }
+}   
